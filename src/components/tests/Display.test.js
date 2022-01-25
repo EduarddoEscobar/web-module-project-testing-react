57ffe1ad9ddd48955a62b8b5b49be4feb4372e06
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-24 19:01:54 -0500

    Through the mvp

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -4,12 +4,57 @@ import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
+import mockFetchShow from '../../api/fetchShow';
+jest.mock('../../api/fetchShow');
 
+
+const exampleShow = {
+    name: 'Show',
+    summary: 'Summary',
+    seasons: [
+        {id: 1, name: 'Season 1', episodes: []},
+        {id: 2, name: 'Season 2', episodes: []},
+        {id: 3, name: 'Season 3', episodes: []}
+    ]
+}
 
 test('renders without errors with no props', ()=>{
-
+    render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(exampleShow);
+    render(<Display />);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+    const button = screen.queryByRole('button');
+    userEvent.click(button);
+
+    const showComponent = await screen.findByTestId(/show-container/i);
+    expect(showComponent).toBeTruthy();
+});
+
+test('renders show season options matching your data when the button is clicked', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(exampleShow);
+    render(<Display />);
+
+    const button = screen.queryByRole('button');
+    userEvent.click(button);
+
+    const options = await screen.findAllByTestId(/season-option/i);
+
+    expect(options.length).toBe(exampleShow.seasons.length);
+});
+
+test('tests when the fetch button is clicked the function is called', async () => {
+    mockFetchShow.mockResolvedValueOnce(exampleShow);
+    const mockDisplayFunc = jest.fn();
+
+    render(<Display displayFunc={mockDisplayFunc} />);
+    const button = screen.queryByRole('button');
+    userEvent.click(button);
+
+    await waitFor(() => {
+        expect(mockDisplayFunc).toBeCalled();
+    })
+
+})
